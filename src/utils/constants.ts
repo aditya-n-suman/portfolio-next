520a938677f2aee81164c30099f85a7dbcb838e9
Aditya Nath Suman <adityanathsuman13@gmail.com>
2024-07-22 13:38:41 +0530

    fix: social media links added

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,6 @@
 export const NAV_ITEMS = ["About", "Experience", "Work", "Contact"];
-export const SOCIAL_MEDIA = ["github", "coffeemug", "linkedin"];
+export const SOCIAL_MEDIA = [
+  { name: "github", link: "https://github.com/aditya-n-suman" },
+  { name: "coffeemug", link: "" },
+  { name: "linkedin", link: "https://www.linkedin.com/in/aditya-nath-suman/" },
+];
